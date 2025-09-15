195356e7b834beeeb9d98dc8e5a4b7458cb12703
ecen0112 <ecen23259@gmail.com>
2025-09-15 19:31:51 +0800

    the

--- u.py
+++ u.py
@@ -13,3 +13,17 @@ string = input("enter a string :")
 s = counted(string)
 for i in sorted(s):
     print(f"{i} = {s[i]}")
+
+
+
+t =[]
+
+for i in range(10):
+    c = int(input("enter a number :"))
+    t.append(c)
+
+t.sort()
+
+print(f"largest :{t[-1]} and the lowest {t[0]}")
+
+
